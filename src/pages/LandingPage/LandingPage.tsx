a8bfd2c26f6e540061270c2a9ce2849143f5810a
Piotr_Berezka <berezkapiotr@gmail.com>
2021-10-25 22:37:52 +0200

    adjust landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -11,20 +11,24 @@ const LandingPage: React.FC = () => {
     <div className="landing-page">
       <Container size="xxl">
         <div className="first-section-container">
-          <div className="left-section">{t("XD")}</div>
+          <div className="left-section">
+            {t("XDdddddddddddddddddddddddddddddddddddddddddddddddddd")}
+          </div>
           <div className="right-section">
-            <div className="first-section-logo">
-              <HestiaLogo size="150" large/>
-            </div>
-            <div className="first-section-content">
-              <span>
-                <h1>{t("landing_page.first_section.main_header_text")}<span className="main-everyone">{t("landing_page.first_section.main_everyone")}</span></h1>
-              </span>
-              <p>
-                {t("landing_page.first_section.main_paragraph_text")}
-              </p>
-              <div>
-                {/* buttons here */}
+            <div className="right-section-container">
+              <div className="first-section-logo">
+                <HestiaLogo size="150" large/>
+              </div>
+              <div className="first-section-content">
+                <span>
+                  <h1>{t("landing_page.first_section.main_header_text")}<span className="main-everyone">{t("landing_page.first_section.main_everyone")}</span></h1>
+                </span>
+                <p>
+                  {t("landing_page.first_section.main_paragraph_text")}
+                </p>
+                <div>
+                  {/* buttons here */}
+                </div>
               </div>
             </div>
           </div>
