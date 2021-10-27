b7bddb5b7675fa07f4274cf5b05cac2b71754c6f
Piotr_Berezka <berezkapiotr@gmail.com>
2021-10-27 17:56:59 +0200

    add and configure tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import LandingPage from './pages/LandingPage/LandingPage';
+import LandingPage from './pages/LandingPage';
 
-import './App.scss';
-import Navbar from './components/Navbar/Navbar';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <Switch>
           <Route path="/" component={LandingPage}/>
         </Switch>
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import './i18n';
-
+import './index.css';
 import App from './App';
 import reducers from './reducers';
 
